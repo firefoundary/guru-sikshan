a2f907ac2cd18c3bd8616c89fecf04f0511da8ca
Advay Singh <153810091+firefoundary@users.noreply.github.com>
2026-01-22 23:35:23 +0530

    Updated supabaseClient.js

--- packages/frontend-dashboard/src/supabaseClient.js
+++ packages/frontend-dashboard/src/supabaseClient.js
@@ -1,16 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const supabaseUrl = 'https://sueztfjexncmfntymjfn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1ZXp0ZmpleG5jbWZudHltamZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4MjkzODUsImV4cCI6MjA4NDQwNTM4NX0.CFbwsDxoQ8YTIzFQLJ3E2d418iz96HEoXtdA0ND8hk8'; 
+const __filename = fileURLToPath(import.meta.url);
+const  _dirname = path.dirname(_filename);
 
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-
-
-// just for debugging don't mind
-console.log("--- SUPABASE DEBUG ---");
-console.log("URL:", supabaseUrl);
-console.log("Key Length:", supabaseKey.length); 
-console.log("Key Starts With:", supabaseKey.substring(0, 15));
-console.log("----------------------");
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient( process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY
+);
